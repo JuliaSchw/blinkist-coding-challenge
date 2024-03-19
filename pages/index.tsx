6f5b2cb02e2b56e28f8b852d8a39fc8c9ce0a795
Julia Schwerdtfeger <js.schwerdt@gmail.com>
2024-03-19 12:37:48 +0100

    setup for code challenge

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,37 @@
 import React from "react";
+import { trackPageview, trackEvent } from "./api/analytics";
+import Image from "next/image";
 
 type HomePageProps = {};
 
 const HomePage: React.FC<HomePageProps> = (props) => {
-  return <h2>test</h2>;
+  return (
+    <>
+      <h1>Check out the Blinkist app</h1>
+
+      <Image
+        width={525}
+        height={275.5}
+        src="/hero_image.jpg"
+        alt="Check out the Blinkist app"
+      />
+
+      <div>
+        {/* Control variation */}
+        Meet the app that revolutionized reading.
+      </div>
+
+      <div>
+        {/* Test variation  */}
+        Meet the app that has 18 million users.
+      </div>
+
+      <div>
+        Thanks a lot for reading the article! <a href="/signup">SIGN UP</a> for
+        Blinkist.
+      </div>
+    </>
+  );
 };
 
 export default HomePage;
