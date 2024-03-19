972073b56278d744f359203f84fcc4e0a4890326
Julia Schwerdtfeger <js.schwerdt@gmail.com>
2024-03-19 12:41:21 +0100

    delete comments

--- pages/api/analytics.ts
+++ pages/api/analytics.ts
@@ -1,31 +1,26 @@
-// Define an interface for the Pageview parameters
 interface PageviewParams {
-    url: string;
-    // Add any other parameters you think might be valuable
-  }
-  
-  // Define an interface for the Event tracking parameters
-  interface EventParams {
-    url: string;
-    eventName: string;
-    // Add any other parameters you think might be valuable
-  }
-  
-  /**
-   * Tracks a pageview to our "imaginary api" - in this demo just the browser console. ;)
-   * Send as params whatever you might seem valuable to send.
-   * The URL is probably a good start though.
-   */
-  export const trackPageview = (params: PageviewParams) => {
-    console.log(`--> Tracking Pageview: ${JSON.stringify(params)}`);
-  };
-  
-  /**
-   * Tracks an event to our "imaginary api" - in this demo just the browser console. ;)
-   * Send as params whatever you might seem valuable to send.
-   * The URL and an event name are probably a good start though.
-   */
-  export const trackEvent = (params: EventParams) => {
-    console.log(`--> Tracking Event: ${JSON.stringify(params)}`);
-  };
-  
+  url: string;
+}
+
+interface EventParams {
+  url: string;
+  eventName: string;
+}
+
+/**
+ * Tracks a pageview to our "imaginary api" - in this demo just the browser console. ;)
+ * Send as params whatever you might seem valuable to send.
+ * The URL is probably a good start though.
+ */
+export const trackPageview = (params: PageviewParams) => {
+  console.log(`--> Tracking Pageview: ${JSON.stringify(params)}`);
+};
+
+/**
+ * Tracks an event to our "imaginary api" - in this demo just the browser console. ;)
+ * Send as params whatever you might seem valuable to send.
+ * The URL and an event name are probably a good start though.
+ */
+export const trackEvent = (params: EventParams) => {
+  console.log(`--> Tracking Event: ${JSON.stringify(params)}`);
+};
